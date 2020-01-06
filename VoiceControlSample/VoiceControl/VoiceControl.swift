6bb3ff7232e1035e63ace7eb0ff2ca6b2230edfd
Soojin Ro <sugarpoint33@gmail.com>
2020-01-06 12:37:45 +0900

    Combine JarvisDelegate into VoiceControl

--- VoiceControlSample/VoiceControl/VoiceControl.swift
+++ VoiceControlSample/VoiceControl/VoiceControl.swift
@@ -10,17 +10,6 @@ import UIKit
 
 @IBDesignable
 public final class VoiceControl: UIControl {
-    internal var clovaAgentView: VoiceAgentView?
-    
-    private func startSpeechRecognizer() {
-        jarvis = MockJarvis()
-        jarvis?.delegate = self
-        jarvis?.start()
-    }
-    
-    private func stopSpeechRecognizer() {
-        jarvis = nil
-    }
     
     public override var inputAccessoryView: UIView? {
         return clovaAgentView
@@ -113,7 +102,75 @@ public final class VoiceControl: UIControl {
         }
     }
     
+    private func startSpeechRecognizer() {
+        jarvis = MockJarvis()
+        jarvis?.delegate = self
+        jarvis?.start()
+    }
+    
+    private func stopSpeechRecognizer() {
+        jarvis = nil
+    }
+    
+    private func hasRecognizedText() -> Bool {
+        if let transcription = clovaAgentView?.transcription {
+            return transcription != listeningStateDescription
+        }
+        
+        return false
+    }
+    
+    private var clovaAgentView: VoiceAgentView?
     private var jarvis: Jarvis?
     private let imageView = UIImageView()
     private let circleLayer = CAShapeLayer()
+    private let listeningStateDescription = "듣고 있어요"
+}
+
+extension VoiceControl: JarvisDelegate {
+    func jarvis(_ jarvis: Jarvis, didChangeState state: JarvisState) {
+        switch state {
+        case .preparing:
+            print("attending")
+            clovaAgentView?.stateView?.setState(.attending)
+        case .listening:
+            print("listening")
+            clovaAgentView?.transcription = listeningStateDescription
+            clovaAgentView?.stateView?.setState(.listening)
+        case .loading:
+            print("processing")
+            clovaAgentView?.transcription = nil
+            clovaAgentView?.stateView?.setState(.processing)
+        case .speaking(let transcript):
+            print("reporting")
+            clovaAgentView?.transcription = nil
+            clovaAgentView?.stateView?.setState(.reporting)
+            clovaAgentView?.transcription = transcript
+        }
+    }
+    
+    func jarvis(_ jarvis: Jarvis, didRecognizeText text: String) {
+        print("detecting")
+        clovaAgentView?.stateView?.setState(.detecting)
+        
+        if clovaAgentView?.stateView?.state == .detecting {
+            clovaAgentView?.transcription = text
+        }
+    }
+    
+    func jarvis(_ jarvis: Jarvis, didChangeAmplitude value: CGFloat) {
+        clovaAgentView?.stateView?.amplify(with: Double(value))
+    }
+    
+    func jarvis(_ jarvis: Jarvis, didFailWithError error: Error) {
+        resignFirstResponder()
+    }
+    
+    func jarvisDidEndRecognizing(_ jarvis: Jarvis) {
+        clovaAgentView?.transcription = nil
+        clovaAgentView?.stateView?.setState(.attending)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+            self.resignFirstResponder()
+        }
+    }
 }
